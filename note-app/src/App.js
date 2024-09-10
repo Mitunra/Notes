5905d3b090c2920faa42f896734b27826e0fdb7c
Mitunra <Mitunra@gmail.com>
2024-09-11 01:10:45 +0530

    basic skull

--- note-app/src/App.js
+++ note-app/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>yello</h1>
+      <h1>React Notes App</h1>
+      <input type="date" name="date"/><br></br>
+      <input type="text" name="note"  placeholder="enter your notes"/>
+      <button>submit</button>
     </div>
   );
 }
